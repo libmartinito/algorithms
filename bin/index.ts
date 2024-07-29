f2e96dd30d8e67de9447ddce135bdb4f40ca2680
libmartinito <lib.martinito@pm.me>
2024-07-29 15:16:28 +0800

    init

---
+++ bin/index.ts
@@ -0,0 +1,12 @@
+import { Command } from "commander";
+
+const program = new Command();
+
+program.description("A CLI for using data structure & algorithm implementations");
+
+program.parse();
+
+
+
+
+
